a160de0f33105f8d89c44f303874d8728222334c
chanwowow <cksdn1425@gmail.com>
2023-03-08 00:37:33 +0900

    data conn change & airplane set panel added

--- RFTestassistapp/app/src/main/java/com/example/rcthelper/FlightToggle.kt
+++ RFTestassistapp/app/src/main/java/com/example/rcthelper/FlightToggle.kt
@@ -3,6 +3,7 @@ package com.example.rcthelper
 import android.provider.Settings
 import android.annotation.SuppressLint
 import android.content.ContentResolver
+import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,16 +16,20 @@ import android.provider.Telephony
 import android.telephony.TelephonyCallback
 import android.telephony.TelephonyCallback.DataConnectionStateListener
 import android.telephony.TelephonyManager
+import android.telephony.ims.ImsReasonInfo
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.annotation.RequiresApi
+import kotlinx.coroutines.delay
 
 
 class FlightToggle : AppCompatActivity() {
 
     private lateinit var printOnScreen: TextView
     private lateinit var telephonyManager: TelephonyManager
-    private lateinit var telephonyCallback: TelephonyCallback
+    private lateinit var telephonyManager2: TelephonyManager
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,42 +42,60 @@ class FlightToggle : AppCompatActivity() {
         val checkInterval = intent.getIntExtra("value", 0) // Main Acticity에서 체크간격 받음
 
         // Get the TelephonyManager
-        telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+        telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        registerTelephonyCallback(telephonyManager)
 
-        // Create a TelephonyCallback to monitor data connection status
-        telephonyCallback = @RequiresApi(Build.VERSION_CODES.S)
-        object : TelephonyCallback(), DataConnectionStateListener {
-            override fun onDataConnectionStateChanged(state: Int, networkType: Int) {
-                when (state) {
-                    TelephonyManager.DATA_CONNECTED -> printOnScreen.text ="Connected CHANWOO"
-                    TelephonyManager.DATA_CONNECTING -> printOnScreen.text ="Connecting CHanwoo"
-                    TelephonyManager.DATA_DISCONNECTED ->printOnScreen.text ="Disconnected CHanwoo"
-                }
-            }
+        // 비행기모드 토글버튼
+        val toggleAirplaneModeButton =findViewById<ToggleButton>(R.id.toggleButton)
+        toggleAirplaneModeButton.setOnClickListener{
+            toggleAirplaneMode()
         }
-        setAirplaneMode(true)
+
+        //setAirplaneMode(true)
         //setAirplaneMode(false)
     }
+    // 아래 코드 reuse 가능하게 바꿔야할듯
+    private fun registerTelephonyCallback(telephonyManager: TelephonyManager){
+        telephonyManager.registerTelephonyCallback(
+            mainExecutor,
+            object : TelephonyCallback(), DataConnectionStateListener {
+                override fun onDataConnectionStateChanged(state: Int, networkType: Int) {
+                    printOnScreen.text ="Changing Something!!!!!####"
 
-    private fun setAirplaneMode(enable : Boolean) {
-        Settings.System.putInt(
-            contentResolver,
-            Settings.Global.AIRPLANE_MODE_ON,
-            if (isEnabled) 0 else 1
-        )
+                    when (state) {
+                        TelephonyManager.DATA_CONNECTED -> {
+                            printOnScreen.text ="Connected CHANWOO"
+                        }
+                        TelephonyManager.DATA_DISCONNECTED -> {
+                            printOnScreen.text ="Disconnected CHanwoo"
+                        }
+                        TelephonyManager.DATA_SUSPENDED -> {
+                            printOnScreen.text ="Suspended CHanwoo"
+                        }
+                    }
+                }
+            })
+    }
 
-        // Send broadcast to notify the system that airplane mode has changed
-        val intent =  Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-        intent.putExtra("state", enable)
-        sendBroadcast(intent)
+    // IMS Call drop 감지 시도
+//    private val imsCallDisconnectListener = object : TelephonyCallback.ImsCallDisconnectCauseListener() {
+//        override fun onImsCallDisconnectCauseChanged(imsReasonInfo: ImsReasonInfo?) {
+//            super.onImsCallDisconnectCauseChanged(imsReasonInfo)
+//            Log.d("IMS_CALL", "IMS call disconnected with reason: ${imsReasonInfo?.code}")
+//            // add your own logic here to handle the IMS call disconnect cause
+//        }
+//    }
 
-        // Prompt the user to grant the "WRITE_SETTINGS" permission
-        if( !Settings.System.canWrite(this)){ // 시스템쓰기 권한이 없다면
-            val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
-            intent.data = Uri.parse("package:$packageName")
-            startActivityForResult(intent,0)
+
+    private fun toggleAirplaneMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val settingsPanelIntent = Intent(Settings.ACTION_AIRPLANE_MODE_SETTINGS)
+            startActivityForResult(settingsPanelIntent, 0)
+        } else {
+            val isAirplaneModeOn = Settings.Global.getInt(contentResolver, Settings.Global.AIRPLANE_MODE_ON, 0) != 0
+            Settings.Global.putInt(contentResolver, Settings.Global.AIRPLANE_MODE_ON, if (isAirplaneModeOn) 0 else 1)
+           // Toast.makeText(this, "Airplane mode has been toggled", Toast.LENGTH_SHORT).show()
         }
-
     }
 
 }
